cb664680cf04fe3b77b88f4ded2909a2cb6db020
martinristow <martinristov084@gmail.com>
2025-05-01 00:06:29 +0200

    testing shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-export default function Home() {
-    return (
+import {Button} from "@/components/ui/button";
+
+const Home = () =>
+    (
         <div>
-            <h1>Hello</h1>
-
-
+            <Button>Click me</Button>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </div>
     );
-}
+
+
+export default Home;
