00a4974403b25ff7808772715802f3624890e5bb
martinristow <martinristov084@gmail.com>
2025-05-01 22:35:10 +0200

    initial commit

---
+++ components/BookList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const BookList = () => {
+    return (
+        <section>
+            <h2 className="font-sans text-4xl text-white">Popular Books</h2>
+        </section>
+    )
+}
+export default BookList
