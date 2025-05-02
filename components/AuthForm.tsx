6cd353d0e53d7914974b479b450c0226c55b4ffa
martinristow <martinristov084@gmail.com>
2025-05-02 18:55:38 +0200

    added type

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,10 +1,11 @@
+
 import React from 'react'
 
 // interface Props<T extends FieldValues>
 
 const AuthForm = ({type, schema, defaultValues, onSubmit}: Props) => {
     return (
-        <div>AuthForm</div>
+        <div>AuthForm -- {type}</div>
     )
 }
 export default AuthForm
