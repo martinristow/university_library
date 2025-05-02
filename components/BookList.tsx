cc4e335362b56520966bbfce09b1708808e9ab30
martinristow <martinristov084@gmail.com>
2025-05-02 12:20:34 +0200

    Book list component that maps all dummy books

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import BookCard from "@/components/BookCard";
 
-const BookList = () => {
+interface Props {
+    books: Book[];
+    containerClassName?: string;
+    title: string;
+}
+
+const BookList = ({title, books, containerClassName}: Props) => {
     return (
-        <section>
-            <h2 className="font-sans text-4xl text-white">Popular Books</h2>
+        <section className={containerClassName}>
+            <h2 className="font-sans text-4xl text-white">{title}</h2>
+
+            <ul className="book-list">
+                {books.map(book => (
+                    <BookCard key={book.title} {...book} />
+                ))}
+            </ul>
         </section>
     )
 }
