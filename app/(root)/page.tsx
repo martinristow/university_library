2393ddc38802ab1b366f8e78682e72ba6bbb9b3b
martinristow <martinristov084@gmail.com>
2025-05-02 12:20:57 +0200

    enhanced a page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,19 @@
 import {Button} from "@/components/ui/button";
 import {BookOverview} from "@/components/BookOverview";
 import BookList from "@/components/BookList";
+import {sampleBooks} from "@/constants";
 
 const Home = () =>
     (
         <div>
-            <BookOverview/>
-            <BookList/>
+            <BookOverview {...sampleBooks[0]}/>
+
+
+            <BookList
+                title="Latest Books"
+                books={sampleBooks}
+                containerClassName="mt-28"
+            />
         </div>
     );
 
