a208450e0d98a162643b6e103649be85ffe45d42
martinristow <martinristov084@gmail.com>
2025-05-03 19:01:15 +0200

    added database url from neon

--- lib/config.ts
+++ lib/config.ts
@@ -4,7 +4,8 @@ const config = {
             urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
             publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
             privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-        }
+        },
+        databaseUrl: process.env.DATABASE_URL,
     }
 }
 
