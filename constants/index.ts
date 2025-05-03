da447c886a386c8a54e8ee65f0e0dd1e66e13a6f
martinristow <martinristov084@gmail.com>
2025-05-03 11:06:47 +0200

    added field names and field types

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,19 @@
+export const FIELD_NAMES = {
+    fullName: "Full name",
+    email: "Email",
+    universityId: "University ID Number",
+    password: "Password",
+    universityCard: "Upload University ID Card",
+}
+
+export const FIELD_TYPES = {
+    fullName: "text",
+    email: "email",
+    universityId: "number",
+    password: "password",
+}
+
+
 export const navigationLinks = [
     {
         href: "/library",
