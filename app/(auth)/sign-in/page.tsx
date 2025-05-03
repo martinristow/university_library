2f9d9e3c61121087b562b90dc7da06829172b685
martinristow <martinristov084@gmail.com>
2025-05-03 11:07:47 +0200

    finished sign in & sign up form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,18 @@
+"use client"
 import React from 'react'
+import AuthForm from "@/components/AuthForm";
+import {singInSchema} from "@/lib/validations";
 
-const Page = () => {
-    return (
-        <div>Sign In Form</div>
-    )
-}
+const Page = () => (
+    <AuthForm
+        type="SIGN_IN"
+        schema={singInSchema}
+        defaultValues={{
+            email: "",
+            password: "",
+        }}
+        onSubmit={() => {
+        }}
+    />
+)
 export default Page
