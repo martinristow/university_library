ebb7056ec44b7197e99f1e8d92695902495a3856
martinristow <martinristov084@gmail.com>
2025-05-04 11:30:31 +0200

    obtaining the user's current IP address and setting a rate limit

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -5,9 +5,17 @@ import {users} from "@/database/schema";
 import {eq} from "drizzle-orm";
 import {hash} from "bcryptjs";
 import {signIn} from "@/auth";
+import {headers} from "next/headers";
+import ratelimit from "@/lib/ratelimit";
+import {redirect} from "next/navigation";
 
 export const signInWithCredentials = async (params: Pick<AuthCredentials, "email" | "password">) => {
     const {email, password} = params;
+
+    const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+    const {success} = await ratelimit.limit(ip);
+
+    if(!success) return redirect("/too-fast")
 
     try {
         const result = await signIn("credentials", {email, password, redirect: false});
@@ -23,6 +31,11 @@ export const signInWithCredentials = async (params: Pick<AuthCredentials, "email
 
 export const signUp = async (params: AuthCredentials) => {
     const {fullName, email, password, universityId} = params;
+
+    const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+    const {success} = await ratelimit.limit(ip);
+
+    if(!success) return redirect("/too-fast")
 
     // check if the user already exists
     const existingUser = await db.select().from(users).where(eq(users.email, email)).limit(1);
