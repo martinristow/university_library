03611ebf662d74300079f5372c4f4941a3bfe8b5
martinristow <martinristov084@gmail.com>
2025-05-16 22:46:06 +0200

    implement youtube frame to test video functionality

--- components/BookVideo.tsx
+++ components/BookVideo.tsx
@@ -1,11 +1,19 @@
 "use client";
 import React from 'react'
-import ReactPlayer from "react-player";
+
+// const convertToEmbedUrl = (url: string) => {
+//     return url.split("v=")[1]
+// }
 
 const BookVideo = ({videoUrl}: {videoUrl: string}) => {
+    // const embedUrl = convertToEmbedUrl(videoUrl)
     return (
         <div>
-            <ReactPlayer url={videoUrl} width="100%" controls />
+            <iframe width="971" height="546" src={`https://www.youtube.com/embed/cpCWyzpHywc`}
+                    title="100 Stephen Curry Shots That Shocked the NBA" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    allowFullScreen></iframe>
         </div>
     )
 }
