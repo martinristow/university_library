c96f72ab535ea902d22264c1941328bfd4d644e5
martinristow <martinristov084@gmail.com>
2025-05-16 22:45:48 +0200

    removed unnecessary spaces and rows

--- app/(root)/books/[id]/page.tsx
+++ app/(root)/books/[id]/page.tsx
@@ -7,7 +7,7 @@ import {BookOverview} from "@/components/BookOverview";
 import {auth} from "@/auth";
 import BookVideo from "@/components/BookVideo";
 
-const Page = async ({params} : {params: Promise<{id: string}>}) => {
+const Page = async ({params}: { params: Promise<{ id: string }> }) => {
 
     const id = (await params).id;
     const session = await auth()
@@ -15,11 +15,11 @@ const Page = async ({params} : {params: Promise<{id: string}>}) => {
     // Fetch data based on id
     const [bookDetails] = await db.select().from(books).where(eq(books.id, id)).limit(1);
 
-    if(!bookDetails) redirect("/404")
+    if (!bookDetails) redirect("/404")
 
     return (
         <>
-        <BookOverview {...bookDetails} userId={session?.user?.id as string} />
+            <BookOverview {...bookDetails} userId={session?.user?.id as string}/>
 
             <div className="book-details">
                 <div className="flex-[1.5]">
@@ -32,7 +32,9 @@ const Page = async ({params} : {params: Promise<{id: string}>}) => {
                     </section>
 
                     <section className="mt-10 flex flex-col gap-7">
-                        <h3>Summary</h3>
+                        <div className="text-white">
+                            <h3>Summary</h3>
+                        </div>
                         <div className="space-y-5 text-xl text-white">
                             {bookDetails.summary}
                         </div>
